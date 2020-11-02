462823b472121c9789c8eb17e50abeeeadd81f60
Yuukoq <kljo10@wp.pl>
2020-11-02 13:10:21 +0100

    pierwsze commit

---
+++ src/xdd.java
@@ -0,0 +1,15 @@
+
+public class xdd {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 15;
+        double sum = a + b;
+
+
+
+
+        System.out.println("wynik = " + sum);
+
+
+    }
+}
