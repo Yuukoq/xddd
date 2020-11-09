842cb0b1e064af0e25b954221c2fb29b6fc5db91
Yuukoq <kljo10@wp.pl>
2020-11-09 02:36:37 +0100

    trzeci commit

---
+++ src/Ptaki/Main.java
@@ -0,0 +1,10 @@
+package Ptaki;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Ptak Ptak1 = new Ptak("Sikorka", "Złółty", "Europa", true, "robaki");
+        Ptak Ptak2 = new Ptak("Pingwin", "Czarnobiały","Anktartyda",true,"Ryby");
+        Ptak Ptak3 = new Ptak("Struś", "Szare","Afryka", true,"robaki");
+    }
+}
---
+++ src/Ptaki/Ptak.java
@@ -0,0 +1,18 @@
+package Ptaki;
+
+public class Ptak {
+    String gatunek;
+    String kolor;
+    String wystepowanie;
+    boolean czytworzygnazda;
+    String dieta;
+
+    Ptak (String gatunek, String kolor, String wystepowanie, boolean czytworzygnazda, String dieta)
+    {
+        this.gatunek = gatunek;
+        this.kolor = kolor;
+        this.wystepowanie = wystepowanie;
+        this.czytworzygnazda = czytworzygnazda;
+        this.dieta= dieta;
+    }
+}
